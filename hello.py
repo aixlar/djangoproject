b7516185433aad0b3c31e8c1ebf3588f28f1f447
aixlar <aixlar6@gmail.com>
2018-08-24 08:33:50 +0300

    2nd task update

--- hello.py
+++ hello.py
@@ -1,7 +1,17 @@
-def app(env, start_response):
-    status  = '200 OK'
-    headers = [('Content-Type', 'text/plain')]
-    body    = env['QUERY_STRING']
-    body    = '\n'.join(body.split('&')).encode('utf-8')
-    start_response(status, headers)
-    return iter([body])
+# def app(env, start_response):
+#     status  = '200 OK'
+#     headers = [('Content-Type', 'text/plain')]
+#     body    = env['QUERY_STRING']
+#     body    = '\n'.join(body.split('&')).encode('utf-8')
+#     start_response(status, headers)
+#     return iter([body])
+
+def app(environ, start_response):
+    data = b'Hello, World!\n'
+    status = '200 OK'
+    response_headers = [
+        ('Content-type', 'text/plain'),
+        ('Content-Length', str(len(data)))
+    ]
+    start_response(status, response_headers)
+    return iter([data])
