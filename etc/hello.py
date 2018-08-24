977466756aa9054b40b9f82a206e341184c8b28b
aixlar <aixlar6@gmail.com>
2018-08-24 08:30:43 +0300

    2nd task update

--- etc/hello.py
+++ etc/hello.py
@@ -1,12 +1,7 @@
 CONFIG = {
-    'mode': 'wsgi',
-    'working_dir': '/home/box/web',
-    'python': '/usr/bin/python3',
     'args': (
-        '--bind=0.0.0.0:8080',
-        '--workers=2',
-        '--timeout=15',
         '--log-level=debug',
+        '--bind=0.0.0.0:8080',
         'hello:app'
     )
 }
