27e12d56f351e52892d008abccb422435eaa8458
aixlar <aixlar6@gmail.com>
2018-08-24 05:23:23 +0300

    added init.sh file and nginx.conf

---
+++ init.sh
@@ -0,0 +1,3 @@
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf     # create symbolic link for nginx.conf in /sites-enabled/ and overwrite test.conf with nginx.conf
+sudo rm -rf /etc/nginx/sites-enabled/default                                    # delete default file
+sudo /etc/init.d/nginx restart                                                  # (re)start nginx server
