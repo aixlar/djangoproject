1779ecaa97e5bd525850e56d9a14acf7b46e5014
aixlar <aixlar6@gmail.com>
2018-08-24 08:04:51 +0300

    2nd task update

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf     # create symbolic link for nginx.conf in /sites-enabled/ and overwrite test.conf with nginx.conf
-sudo rm -rf /etc/nginx/sites-enabled/default                                    # delete default file
-sudo /etc/init.d/nginx restart                                                  # (re)start nginx server
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py                  # 
-sudo /etc/init.d/gunicorn restart                                               #
+sudo rm /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart 
+sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart
