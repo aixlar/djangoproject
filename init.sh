8c1808e1deba237f3458c22056ad40238dbd6de0
aixlar <aixlar6@gmail.com>
2018-08-24 07:58:31 +0300

    changed init script

--- init.sh
+++ init.sh
@@ -1,7 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf     # create symbolic link for nginx.conf in /sites-enabled/ and overwrite test.conf with nginx.conf
 sudo rm -rf /etc/nginx/sites-enabled/default                                    # delete default file
 sudo /etc/init.d/nginx restart                                                  # (re)start nginx server
-sudo ln -s /home/box/etc/hello.py /etc/gunicorn.d/hello.py                      # 
+sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py                  # 
 sudo /etc/init.d/gunicorn restart                                               #
-cd /home/box/web/                                                               #
-gunicorn hello:app --bind 0.0.0.0:8080                                          #        
