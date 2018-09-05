93418aba5d6b154276dc8fb9664fd97c8aac9421
aixlar <aixlar6@gmail.com>
2018-09-05 15:40:28 +0300

    update

--- etc/hello.py
+++ etc/hello.py
@@ -1,10 +1,3 @@
-CONFIG = {
-    'mode':         'wsgi',
-    'working_dir':  '/home/box/web/',
-    # 'python':       '/usr/bin/python',
-    'args': (
-        '--bind=0.0.0.0:8000',
-        '--access-logfile "-"',
-        'hello:app',
-    ),
-}
+# pythonpath      = '/home/box/web'
+bind            = '0.0.0.0:8080'
+workers         = 4
--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
-sudo rm -rf /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn restart --chdir /home/box/web/ hello:app 
-# sudo /etc/init.d/gunicorn restart
-# -c /opt/myenv/gunicorn_config.py myproject.wsgi
+sudo /etc/init.d/gunicorn restart
+
+gunicorn --bind 0.0.0.0:8080 hello:app &
