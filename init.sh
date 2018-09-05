28a27a04ab0fd55aefbd48da4d2aa5b285d03743
aixlar <aixlar6@gmail.com>
2018-09-05 13:21:22 +0300

    update

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+alias python=python3                                                            #
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default       #
 sudo /etc/init.d/nginx restart                                                  #
 sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py               #
