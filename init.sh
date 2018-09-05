f83ff310a160b0b433450070b447f5b94e3f0156
aixlar <aixlar6@gmail.com>
2018-09-05 13:54:32 +0300

    update

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@ alias python=python3                                                            
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default       #
 sudo /etc/init.d/nginx restart                                                  #
 # sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py             #
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart                                               #
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py                 #
+sudo /etc/gunicorn.d/hello.py restart                                           #
