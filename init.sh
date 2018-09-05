0d07b2685498a2aca13b2dbef4eb966cf90851ad
aixlar <aixlar6@gmail.com>
2018-09-05 16:13:29 +0300

    1.9 done

--- init.sh
+++ init.sh
@@ -1,17 +1,5 @@
-# sudo ln -sf source soft-link
-
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
 
 sudo /etc/init.d/nginx    restart
-sudo /etc/init.d/gunicorn restart
-
-# sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-# sudo /etc/init.d/nginx restart
-#
-# sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-# sudo /etc/init.d/gunicorn restart
-#
-# cd /home/box/web/
-# gunicorn --bind 0.0.0.0:8080 hello:app &
-# # gunicorn --bind 0.0.0.0:8080 hello:app &
+sudo /etc/init.d/gunicorn restart 
