37bbee7800be5eebda261f8d352a1c64a8dc7e04
aixlar <aixlar6@gmail.com>
2018-09-05 15:03:37 +0300

    update

--- init.sh
+++ init.sh
@@ -2,5 +2,6 @@ sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn restart
+sudo /etc/init.d/gunicorn restart --chdir /home/box/web/ hello:app 
+# sudo /etc/init.d/gunicorn restart
 # -c /opt/myenv/gunicorn_config.py myproject.wsgi
